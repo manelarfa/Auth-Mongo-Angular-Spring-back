15f9ed4e0976a332eff6be500a05908aa973ad5c
manel <arfamanelarfa@gmail.com>
2020-11-28 17:50:00 +0100

    first commit

---
+++ src/test/java/com/djamware/springangularauth/SpringAngularAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djamware.springangularauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
